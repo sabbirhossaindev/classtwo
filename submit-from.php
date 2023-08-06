0c2e0961ee829d401d263ae72c7255e449ecaec0
sabbir hossain <mdsabbir477470@gmail.com>
2023-08-07 03:26:56 +0600

    classtwo some work done

---
+++ submit-from.php
@@ -0,0 +1,26 @@
+<?php
+
+// name data
+echo"First Name: ". $_POST['user_first_name'];
+echo "<br>";
+echo"Middle Name: ". $_POST['user_middle_name'];
+echo "<br>";
+echo "Last Name: ". $_POST['user_last_name'];
+
+echo "<br>";
+// Birth day
+echo"Birthday Month: ". $_POST['user_birthday_month'];
+echo"Birthday Date: ". $_POST['user_birthday_date'];
+echo"Birthday Year: ". $_POST['user_birthday_year'];
+echo "<br>";
+//phone
+echo"Phone Number: ". $_POST['user_phone_number'];
+echo "<br>";
+// email
+echo"E-mail Address: ". $_POST['user_email'];
+echo "<br>";
+//
+// echo $_POST['user_phone_number'];
+// echo $_POST['user_phone_number'];
+
+?>
