9f6e82c1d906989e3f46241ec145d9938ef7b6a1
sabbir hossain <mdsabbir477470@gmail.com>
2023-08-07 10:26:52 +0600

    some date added

--- admission-from.php
+++ admission-from.php
@@ -8,59 +8,73 @@
 </head>
 
 <body>
-    <h2>Admission From by Freelancer Sabbir</h2>
+    <h2>Admission From by Freelancer Sabbir.</h2>
+
+    <form action="submit-from.php" method="POST">
+
+        <h3>Name Field</h3>
+        <label for="user_first_name">First name</label><br>
+        <input type="text" name="user_first_name" placeholder="first name"><br>
+
+        <label for="user_middle_name">Middle name</label><br>
+        <input type="text" name="user_middle_name" placeholder="middle name"><br>
+
+        <label for="user_last_name">Last name</label><br>
+        <input type="text" name="user_last_name" placeholder="last name"><br>
+
+        <h4>Birthday Field</h4>
+        <label for="user_birthday_month">Birthday Month name</label><br>
+        <input type="text" name="user_birthday_month" placeholder="month name"><br>
+
+        <label for="user_birthday_date">Birthday Date</label><br>
+        <input type="text" name="user_birthday_date" placeholder="date name"><br>
+
+        <label for="user_birthday_year">Birthday year</label><br>
+        <input type="text" name="user_birthday_year" placeholder="year name"><br>
+
+        <h3>Gender</h3>
+        <input type="checkbox" id="male" name="male" value="male">
+        <label for="male">male</label><br>
+
+        <input type="checkbox" id="female" name="female" value="female">
+        <label for="female">female</label><br>
+
+        <h4>Of which country are you a citizen?</h4>
+        <select id="country_citizen">
+            <option value="Bangladesh" selected>Bangladesh</option>
+            <option value="India">India</option>
+            <option value="London">London</option>
+            <option value="America">America</option>
+            <option value="Brazil">Brazil</option>
+            <option value="Germane">Germane</option>
+            <option value="Argentina">Argentina</option>
+        </select> <br>
+        <br>
+
+        <label for="user_phone_number">Phone Number</label><br>
+        <input type="text" name="user_phone_number" placeholder="enter number"><br>
+        <br>
+        <label for="user_email">Email</label><br>
+        <input type="email" name="user_email" placeholder="example@example.com"><br>
+
+        <h4>Mailing Address</h4>
+        <label for="user_present_address">Type Present Address</label><br>
+        <input type="text" name="user_present_address" placeholder="enter your address"><br>
+
+        <label for="user_permanent_address">Type Permanent Address</label><br>
+        <input type="text" name="user_permanent_address" placeholder="enter your address"><br>
+
+        <label for="user_city">City</label><br>
+        <input type="text" name="user_city" placeholder="enter your city"><br>
+
+        <label for="user_state">State / Province</label><br>
+        <input type="text" name="user_state" placeholder="enter you state"><br>
+
+        <br>
+        <br>
+        <input class="" type="submit" value="Submit">
+    </form>
 </body>
-<form action="submit-from.php" method="POST">
-
-    <h3>Name Field</h3>
-    <label for="user_first_name">First name</label><br>
-    <input type="text" name="user_first_name" placeholder="first name"><br>
-
-    <label for="user_middle_name">Middle name</label><br>
-    <input type="text" name="user_middle_name" placeholder="middle name"><br>
-
-    <label for="user_last_name">Last name</label><br>
-    <input type="text" name="user_last_name" placeholder="last name"><br>
-
-    <h4>Birthday Field</h4>
-    <label for="user_birthday_month">Birthday Month name</label><br>
-    <input type="text" name="user_birthday_month" placeholder="month name"><br>
-
-    <label for="user_birthday_date">Birthday Date</label><br>
-    <input type="text" name="user_birthday_date" placeholder="date name"><br>
-
-    <label for="user_birthday_year">Birthday year</label><br>
-    <input type="text" name="user_birthday_year" placeholder="year name"><br>
-
-    <h3>Gender</h3>
-    <input type="checkbox" id="male" name="male" value="male">
-    <label for="male">male</label><br>
-
-    <input type="checkbox" id="female" name="female" value="female">
-    <label for="female">female</label><br>
-
-    <h4>Of which country are you a citizen?</h4>
-    <select id="country_citizen">
-        <option value="Bangladesh" selected>Bangladesh</option>
-        <option value="India">India</option>
-        <option value="London">London</option>
-        <option value="America">America</option>
-        <option value="Brazil">Brazil</option>
-        <option value="Germane">Germane</option>
-        <option value="Argentina">Argentina</option>
-    </select> <br>
-    <br>
-
-    <label for="user_phone_number">Phone Number</label><br>
-    <input type="text" name="user_phone_number" placeholder="enter number"><br>
-    <br>
-    <label for="user_email">Email</label><br>
-    <input type="email" name="user_email" placeholder="example@example.com"><br>
-
-    <br>
-    <br>
-    <input class="" type="submit" value="Submit">
-</form>
 
 </html>
 
--- submit-from.php
+++ submit-from.php
@@ -1,26 +1,44 @@
 <?php
 
 // name data
-echo"First Name: ". $_POST['user_first_name'];
+echo"First Name: " . $_POST['user_first_name'];
 echo "<br>";
-echo"Middle Name: ". $_POST['user_middle_name'];
+echo"Middle Name: " . $_POST['user_middle_name'];
 echo "<br>";
-echo "Last Name: ". $_POST['user_last_name'];
+echo "Last Name: " . $_POST['user_last_name'];
 
 echo "<br>";
 // Birth day
-echo"Birthday Month: ". $_POST['user_birthday_month'];
-echo"Birthday Date: ". $_POST['user_birthday_date'];
-echo"Birthday Year: ". $_POST['user_birthday_year'];
+echo"Birthday Month: " . $_POST['user_birthday_month'];
+echo"Birthday Date: " . $_POST['user_birthday_date'];
+echo"Birthday Year: " . $_POST['user_birthday_year'];
 echo "<br>";
+
 //phone
-echo"Phone Number: ". $_POST['user_phone_number'];
+echo"Phone Number: " . $_POST['user_phone_number'];
 echo "<br>";
+
 // email
-echo"E-mail Address: ". $_POST['user_email'];
+echo"E-mail Address: " . $_POST['user_email'];
 echo "<br>";
+
+// Select Present Address
+echo"Select Present Address: " .  $_POST['user_present_address'];
+echo "<br>";
+
+// Select Permanent Address
+echo"Select Permanent Address: " .  $_POST['user_permanent_address'];
+echo "<br>";
+
+// Enter your city
+echo"Enter your city: " .  $_POST['user_city'];
+echo "<br>";
+
+// Enter you state
+echo"Enter you state: " .  $_POST['user_state'];
+echo "<br>";
+
 //
-// echo $_POST['user_phone_number'];
 // echo $_POST['user_phone_number'];
 
 ?>
